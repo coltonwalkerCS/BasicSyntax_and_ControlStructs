95797fc9c9908b919ff1b7b166352849839c0977
colewalker <cmw9nb@mst.edu>
2023-08-24 14:05:18 -0500

    Added game function, and random bot player

--- tic_tac_toe_game.h
+++ tic_tac_toe_game.h
@@ -52,10 +52,10 @@ public:
     }
 
     // Given the current board determine if there is a winner
-    int is_winner() {
+    char is_winner() {
         // Need 5 or more moves
         if (this->moves.size() < 5) {
-            return 0;
+            return ' ';
         }
 
         // Check rows for 3 in a row
@@ -65,9 +65,9 @@ public:
             if (currRow[0] == currRow[1] and currRow[1] == currRow[2]) {
                 // If X return player 1 won else player 2
                 if (currRow[0] == 'X') {
-                    return 1;
+                    return 'X';
                 } else {
-                    return 2;
+                    return 'O';
                 }
             }
         }
@@ -83,9 +83,9 @@ public:
             if (currCol[0] == currCol[1] and currCol[1] == currCol[2]) {
                 // If X return player 1 won else player 2
                 if (currCol[0] == 'X') {
-                    return 1;
+                    return 'X';
                 } else {
-                    return 2;
+                    return 'O';
                 }
             }
         }
@@ -93,37 +93,31 @@ public:
         // Check backward diagonal (top left to bottom right)
         if (this->board[0][0] == this->board[1][1] and this->board[1][1] == this->board[2][2]) {
             if (this->board[0][0] == 'X') {
-                return 1;
+                return 'X';
             } else {
-                return 2;
+                return 'O';
             }
         }
 
         // Check forward diagonal (bottom left to top right)
         if (this->board[2][0] == this->board[1][1] and this->board[1][1] == this->board[0][2]) {
             if (this->board[0][0] == 'X') {
-                return 1;
+                return 'X';
             } else {
-                return 2;
+                return 'O';
             }
         }
 
         // If all elements are filled and there is no
         // winner then it's a tie!
         if (this->moves.size() == 9) {
-            return 3;
+            return 'T';
         }
 
-        return 0;
+        return ' ';
     }
 
-    void makeMove(int row, int col, int player) {
-        // Set to player 1 else change
-        char move = 'X';
-        if (player == 2) {
-            move = 'O';
-        }
-
+    void makeMove(int row, int col, char player) {
         // Set the MovePair
         MovePair moveLoc;
         moveLoc.row = row;
@@ -131,7 +125,7 @@ public:
 
         this->moves.push_back(moveLoc);
 
-        this->board[row][col] = move;
+        this->board[row][col] = player;
     }
 
     MovePair lastMove() {
@@ -145,25 +139,27 @@ public:
         return false;
     }
 
-    vector<vector<int>> getAllLegalMoves() {
-        vector<vector<int>> legalMoves;
+    vector<MovePair> getAllLegalMoves() {
+        vector<MovePair> legalMoves;
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 if (this->board[row][col] == '.'){
-                    vector<int> legalMove = {row, col};
-                    legalMoves.push_back(legalMove);
+                    MovePair newMove;
+                    newMove.row = row;
+                    newMove.col = col;
+                    legalMoves.push_back(newMove);
                 }
             }
         }
         return legalMoves;
     }
 
-//    TicTacToeBoard copy() {
-//        TicTacToeBoard newBoard;
-//        newBoard.board = this->board;
-//        newBoard.moves = this->moves;
-//        return newBoard;
-//    }
+    TicTacToeBoard copy() {
+        TicTacToeBoard newBoard;
+        newBoard.board = this->board;
+        newBoard.moves = this->moves;
+        return newBoard;
+    }
 };
 
 class RandomBot {
@@ -171,16 +167,17 @@ private:
     int player;
 
 public:
-    RandomBot(int player_bot) {
-        player = player_bot;
+    RandomBot(int player_type) {
+        player = player_type;
     }
 
     MovePair SelectMove(TicTacToeBoard board) {
         MovePair newMove;
-        vector<vector<int>> legalMoves = board.getAllLegalMoves();
-        int randMoveLoc = getRandomNumber(0, legalMoves.size());
-        newMove.row = legalMoves[randMoveLoc][0];
-        newMove.col = legalMoves[randMoveLoc][1];
+        vector<MovePair> legalMoves = board.getAllLegalMoves();
+        int numLegalMoves = static_cast<int>(legalMoves.size());
+        int randMoveLoc = getRandomNumber(0, numLegalMoves);
+        newMove.row = legalMoves[randMoveLoc].row;
+        newMove.col = legalMoves[randMoveLoc].col;
         return newMove;
     }
 
@@ -197,6 +194,99 @@ void ticTacToeMenuDisplay() {
     cout << "3: Exit \n" << endl;
 }
 
+char swapPlayers(char currPlayer) {
+    char newPlayer;
+    if (currPlayer == 'X'){
+        newPlayer = '0';
+    }else{
+        newPlayer = 'X';
+    }
+    return newPlayer;
+}
+
+// Game logic
+void PlayGame(char playerPiece){
+    // Set bot piece
+    char botPiece = 'X';
+    if (playerPiece == 'X') {
+        botPiece = 'O';
+    }
+
+    TicTacToeBoard board = TicTacToeBoard();
+    RandomBot RandoBot(botPiece);
+
+    char currentPlayer = 'X';
+    char winner;
+
+    for(int round = 0; round < 9; round++){
+        MovePair currentMove;
+
+        if (playerPiece == currentPlayer) {
+            bool isValidMove;
+            do {
+                // Set to false then find if valid
+                isValidMove = false;
+
+                cout << "Input your move." << endl;
+                cout << "Row:" << endl;
+                cin >> currentMove.row;
+                cout << "Col:" << endl;
+                cin >> currentMove.col;
+
+                // Check is valid move
+                // TODO: FIX FOR LOOP? - Not sure what range-based is
+                vector<MovePair> legalMoves = board.getAllLegalMoves();
+                for (int move = 0; move < legalMoves.size(); move++) {
+                    if (legalMoves[move].row == currentMove.row && legalMoves[move].col == currentMove.col ){
+                        isValidMove = true;
+                        break;
+                    }
+                }
+
+            } while (!isValidMove);
+        } else {
+            currentMove = RandoBot.SelectMove(board);
+        }
+
+        // Update board & display
+        board.makeMove(currentMove.row, currentMove.col, currentPlayer);
+        board.displayTicTacToeBoard();
+
+        // Check if there is a winner
+        winner = board.is_winner();
+        if (winner != ' ') {
+            // Winner has been found exit loop
+            break;
+        }
+
+        // Get next player
+        currentPlayer = swapPlayers(currentPlayer);
+
+    }
+    // Display the winner
+    if (winner == 'T') {
+        cout << "The game was a TIE!" << endl;
+    } else {
+        cout << "The winner is player " << winner << "!" << endl;
+    }
+    cout << "Thanks for playing!" << endl;
+}
+
+char getPlayerPiece(){
+    char playerPiece;
+    bool validChoice;
+    do {
+        validChoice = true;
+        cout << "Which piece do you want to play as? (X or O)" << endl;
+        cin >> playerPiece;
+
+        if (!(playerPiece == 'X' || playerPiece == 'O')) {
+            cout << "Invalid piece" << endl;
+            validChoice = false;
+        }
+    } while (!validChoice);
+    return playerPiece;
+}
 
 // Desc: Tic Tac Toe game prompt logic
 // Input: N/A
@@ -206,15 +296,21 @@ void main_TicTacToe(){
     int menuChoice;
 
     do {
+        char playerPiece;
+
         ticTacToeMenuDisplay();
         cin >> menuChoice;
 
-        cout << "Test printing board" << endl;
-        TicTacToeBoard ticTacToe = TicTacToeBoard();
-        ticTacToe.displayTicTacToeBoard();
+        switch (menuChoice) {
+            case 1:
+                playerPiece = getPlayerPiece();
+                PlayGame(playerPiece);
+                break;
+            case 2:
+                cout << "Perfect bot is currently not available" << endl;
+                break;
+        }
 
     } while (menuChoice != 3);
 
-    cout << "You guessed it!" << endl;
-    cout << "Great Job! \n" << endl;
 }
